9488540b2ae98397f5918b2dfdadaf1a2ae302b9
Glitch (giveawy-bot-) <none>
2021-03-02 15:47:30 +0000

    Added read.me // Cleaned up code

--- index.js
+++ index.js
@@ -1,55 +1,48 @@
 const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
 const bot = new Discord.Client();
-const colors = require("./colors")
 const fs = require("fs");
 let prefix = botconfig.prefix;
-let ownerID = '283312847478325251';
+let ownerID = "283312847478325251";
 const active = new Map();
-var servers = {};
 
 bot.commands = new Discord.Collection();
 bot.aliases = new Discord.Collection();
 
-const { GiveawaysManager } = require('discord-giveaways');
+const { GiveawaysManager } = require("discord-giveaways");
 bot.giveawaysManager = new GiveawaysManager(bot, {
-    storage: "./giveaways.json",
-    updateCountdownEvery: 5000,
-    default: {
-        botsCanWin: false,
-        exemptPermissions: [ "ADMINISTRATOR" ],
-        embedColor: "#FF0000",
-        reaction: "🎉"
-    }
+  storage: "./giveaways.json",
+  updateCountdownEvery: 5000,
+  default: {
+    botsCanWin: false,
+    exemptPermissions: [""],
+    embedColor: "#FF0000",
+    reaction: "🎉"
+  }
 });
-
 
 fs.readdir("./commands/", (err, files) => {
-  
-  if(err) console.log(err);
-  let jsfile = files.filter(f => f.split(".").pop() === "js")
-  if(jsfile.length <= 0) {
-    console.log("[LOG] Couldn't find any commands")
+  if (err) console.log(err);
+  let jsfile = files.filter(f => f.split(".").pop() === "js");
+  if (jsfile.length <= 0) {
+    console.log("[LOG] Couldn't find any commands");
   }
 
-
-  jsfile.forEach((f, i) =>{
-  let props = require (`./commands/${f}`);
-  console.log(`${f} loaded!`)
-  bot.commands.set(props.help.name, props);
-});
-  
+  jsfile.forEach((f, i) => {
+    let props = require(`./commands/${f}`);
+    console.log(`${f} loaded!`);
+    bot.commands.set(props.help.name, props);
+  });
 });
 
 bot.on("ready", async () => {
-  console.log(`SkyClouds is online!`)
-  bot.user.setActivity("SkyClouds", {type: "PLAYING"});
-})
+  console.log(`GSBot is online!`);
+  bot.user.setActivity("=help for commands!", { type: "PLAYING" });
+});
 
 bot.on("message", async message => {
-  if(message.author.bot) return;
-  if(message.channel.type === "dm") return;
-  
+  if (message.author.bot) return;
+  if (message.channel.type === "dm") return;
 
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0];
@@ -57,11 +50,11 @@ bot.on("message", async message => {
   let ops = {
     ownerID: ownerID,
     active: active
-  }
-  
-  let commandfile = bot.commands.get(cmd.slice(prefix.length));
-  if(commandfile && message.content[0] === prefix) commandfile.run(bot, message, args, ops);
-})
-       
+  };
 
-bot.login(process.env.token);
+  let commandfile = bot.commands.get(cmd.slice(prefix.length));
+  if (commandfile && message.content[0] === prefix)
+    commandfile.run(bot, message, args, ops);
+});
+
+bot.login(botconfig.token);
---
+++ readme.md
@@ -0,0 +1,10 @@
+<title>
+  "Giveaway Bot Framework"
+</title>
+<p3>
+  You can use the simple bot framework to create your own giveaway bot.
+  To use:
+       1. Clone the GitHub repository
+       2. Edit the token to your Discord bot's token inside of the botconfig.json file.
+       3. You can edit it as you want!
+</p3>
